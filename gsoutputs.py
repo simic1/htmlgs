3d425d8b7db793f2f2384fca60f57c1533cac771
simi <simonc@macroscopia.com>
2018-02-05 07:25:20 +0200

    tesr  correction test
    esr

--- gsoutputs.py
+++ gsoutputs.py
@@ -7,7 +7,9 @@ import pint
 
 
 class GSoutputs:
-    def __init__(self, version, modelname, simtime, exptime, variablename, path, description, nosteps, noreal, weights, variables, timeseries):
+    def __init__(self, version, modelname, simtime, exptime, variablename,
+                 path, description, nosteps, noreal, weights, variables,
+                 timeseries):
         self.version = version
         self.modelname = modelname
         self.simtime = simtime
@@ -22,7 +24,7 @@ class GSoutputs:
         self.timeseries = timeseries
 
     # add core funtions that should be used on a time series (conversion units)
-    def __stats__(self, other):  
+    def __stats__(self, other):
         return 2
 
 
@@ -44,8 +46,10 @@ def readFile(filename):
     numVar = math.floor((length-10)/file[7])
     variables = []
     for i in range(numVar):
-        df = pd.read_csv(filename, skiprows=10+i*(file[7]+4), header=None, nrows=2)
-        variables.append({'variable': df[0][0].split(":", 1)[1], 'unit': df[0][1].split(":", 1)[1]})
+        df = pd.read_csv(filename, skiprows=10+i*(file[7]+4), header=None,
+                         nrows=2)
+        variables.append({'variable': df[0][0].split(":", 1)[1], 'unit':
+                         df[0][1].split(":", 1)[1]})
     variables = pd.DataFrame(variables)
     file.append(variables)
 
@@ -59,7 +63,8 @@ def readFile(filename):
 
     df_compiled = ""
     for i in range(numVar):
-        df = pd.read_table(filename, names=header, skiprows=14+i*(file[7]+4), nrows=file[7], sep='\t') 
+        df = pd.read_table(filename, names=header, skiprows=14+i*(file[7]+4),
+                           nrows=file[7], sep='\t')
         df.drop(columns=file[8]+1, inplace=True)
         df['date'] = pd.to_datetime(df.date,  format='%d/%m/%Y %H:%M:%S')
         df = pd.melt(df, id_vars='date')
@@ -70,20 +75,21 @@ def readFile(filename):
         if i == 0:
             df_compiled = df
         else:
-            df_compiled = df_compiled.merge(df, how="inner")     
-    tuples = list(zip(df_compiled['date'],df_compiled['real']))
+            df_compiled = df_compiled.merge(df, how="inner")
+    tuples = list(zip(df_compiled['date'], df_compiled['real']))
     index = pd.MultiIndex.from_tuples(tuples, names=['date', 'real'])
     df_compiled.set_index(index, drop=True, inplace=True)
     df_compiled.drop(columns=['date', 'real'], inplace=True)
     file.append(df_compiled)
-    output = GSoutputs(file[0], file[1], file[2], file[3], file[4], file[5], file[6], file[7], file[8], file[9], file[10], file[11])
+    output = GSoutputs(file[0], file[1], file[2], file[3], file[4], file[5],
+                       file[6], file[7], file[8], file[9], file[10], file[11])
     return output
 
 # main
 
 
 def Main():
-#    ts = readTS()
+    #    ts = readTS()
     filename = 'data/MultipleOutputs.txt'
     ds = readFile(filename)
     df = ds.timeseries
